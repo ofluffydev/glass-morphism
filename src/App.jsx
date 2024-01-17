9b457299739c475459cd4411b031382e3a545067
Kaden Frisk <fufferpish@gmail.com>
2024-01-17 08:20:38 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './scss/Styles.scss'
+
+function App() {
+    return (<>
+            <div className="center landing-container">
+                <div className="landing-box">
+                    <h1>Glass morphism</h1>
+                    <p>Explore how glass morphism styles can modernize your website</p>
+                </div>
+            </div>
+        </>)
+}
+
+export default App
